ba3514a8ae346c06516fff551f7e9644c2f5770a
JJunee96 <wnsltkak@naver.com>
2023-04-04 17:12:39 +0900

    Update Program.cs

--- Day01/Day01App/cs09_nullconditions/Program.cs
+++ Day01/Day01App/cs09_nullconditions/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace cs09_nullconditions
@@ -10,6 +11,25 @@ namespace cs09_nullconditions
     {
         static void Main(string[] args)
         {
+            Foo myfoo = null;   // new Foo();
+            //myfoo.member = 30;
+
+            //int? bar;
+            //if (myfoo != null)
+            //{
+            //    bar = myfoo.member;
+            //}
+            //else
+            //{
+            //    bar = null;
+            //}
+            
+            int? bar = myfoo?.member;   // 위의 아홉줄(주석부분)을 모두 축약
         }
+    }
+
+    class Foo
+    {
+        public int member;
     }
 }
